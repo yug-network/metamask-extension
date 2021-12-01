d728f3249e33020f8c7a9c46f086126b9464940b
David Walsh <davidwalsh83@gmail.com>
2021-12-01 14:15:03 -0600

    Add hardware connectivity functionality

--- ui/pages/confirm-transaction-base/confirm-transaction-base.container.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.container.js
@@ -12,6 +12,7 @@ import {
   getNextNonce,
   tryReverseResolveAddress,
   setDefaultHomeActiveTabName,
+  connectHardware,
 } from '../../store/actions';
 import { isBalanceSufficient, calcGasTotal } from '../send/send.utils';
 import { shortenAddress, valuesFor } from '../../helpers/utils/util';
@@ -283,6 +284,8 @@ export const mapDispatchToProps = (dispatch) => {
       dispatch(updateTransactionGasFees({ ...gasFees, expectHexWei: true }));
     },
     showBuyModal: () => dispatch(showModal({ name: 'DEPOSIT_ETHER' })),
+    connectHardwareWallet: (deviceName) =>
+      dispatch(connectHardware(deviceName)),
   };
 };
 
