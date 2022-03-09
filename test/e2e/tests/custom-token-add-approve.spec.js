669f4cd6423c25f84071aab499b6a55e22ff10fb
mirjanaKukic <mirjana.kukic@endava.com>
2022-03-09 15:31:50 +0100

    changed test for create, import and renders balance for new token

--- test/e2e/tests/custom-token-add-approve.spec.js
+++ test/e2e/tests/custom-token-add-approve.spec.js
@@ -16,7 +16,8 @@ describe('Add a custom token from a dapp', function () {
       },
     ],
   };
-  it('connects the dapp', async function () {
+
+  it('creates, imports and renders the balance for the new token', async function () {
     await withFixtures(
       {
         dapp: true,
@@ -29,151 +30,28 @@ describe('Add a custom token from a dapp', function () {
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
 
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        await driver.closeAllWindowHandlesExcept([extension]);
-        await driver.clickElement('.app-header__logo-container');
+    windowHandles = await driver.getAllWindowHandles();
+    extension = windowHandles[0];
+    await driver.closeAllWindowHandlesExcept([extension]);
+    await driver.clickElement('.app-header__logo-container');
 
-        // connects the dapp
-        await driver.openNewPage('http://127.0.0.1:8080/');
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(3);
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        dapp = await driver.switchToWindowWithTitle(
-          'E2E Test Dapp',
-          windowHandles,
-        );
-        popup = windowHandles.find(
-          (handle) => handle !== extension && handle !== dapp,
-        );
-        await driver.switchToWindow(popup);
-        await driver.clickElement({ text: 'Next', tag: 'button' });
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-      },
+    // connects the dapp
+    await driver.openNewPage('http://127.0.0.1:8080/');
+    await driver.clickElement({ text: 'Connect', tag: 'button' });
+    await driver.waitUntilXWindowHandles(3);
+    windowHandles = await driver.getAllWindowHandles();
+    extension = windowHandles[0];
+    dapp = await driver.switchToWindowWithTitle(
+      'E2E Test Dapp',
+      windowHandles,
     );
-  });
-
-  it('creates a new token', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: 'imported-account',
-        ganacheOptions,
-        title: this.test.title,
-      },
-      async ({ driver }) => {
-        await driver.navigate();
-        await driver.fill('#password', 'correct horse battery staple');
-        await driver.press('#password', driver.Key.ENTER);
-
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        await driver.closeAllWindowHandlesExcept([extension]);
-        await driver.clickElement('.app-header__logo-container');
-
-        // connects the dapp
-        await driver.openNewPage('http://127.0.0.1:8080/');
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(3);
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        dapp = await driver.switchToWindowWithTitle(
-          'E2E Test Dapp',
-          windowHandles,
-        );
-        popup = windowHandles.find(
-          (handle) => handle !== extension && handle !== dapp,
-        );
-
-        await driver.switchToWindow(popup);
-        await driver.clickElement({ text: 'Next', tag: 'button' });
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(2);
-        await driver.switchToWindow(dapp);
-        // create token
-        await driver.waitForSelector({ text: 'Create Token', tag: 'button' });
-        await driver.clickElement({ text: 'Create Token', tag: 'button' });
-        windowHandles = await driver.waitUntilXWindowHandles(3);
-
-        popup = windowHandles[2];
-        await driver.switchToWindow(popup);
-        await driver.clickElement({ text: 'Edit', tag: 'button' });
-        const inputs = await driver.findElements('input[type="number"]');
-        const gasLimitInput = inputs[0];
-        const gasPriceInput = inputs[1];
-        await gasLimitInput.fill('4700000');
-        await gasPriceInput.fill('20');
-        await driver.waitForSelector({ text: 'Save', tag: 'button' });
-        await driver.clickElement({ text: 'Save', tag: 'button' });
-        await driver.clickElement({ text: 'Confirm', tag: 'button' });
-
-        await driver.switchToWindow(dapp);
-
-        await driver.waitForSelector({
-          css: '#tokenAddress',
-          text: '0x',
-        });
-      },
+    popup = windowHandles.find(
+      (handle) => handle !== extension && handle !== dapp,
     );
-  });
-
-  it('clicks on the import tokens button', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: 'imported-account',
-        ganacheOptions,
-        title: this.test.title,
-      },
-      async ({ driver }) => {
-        await driver.navigate();
-        await driver.fill('#password', 'correct horse battery staple');
-        await driver.press('#password', driver.Key.ENTER);
-
-        // imports token from extension
-        await driver.clickElement(`[data-testid="home__asset-tab"]`);
-        await driver.clickElement({ text: 'import tokens', tag: 'a' });
-      },
-    );
-  });
-
-  it('picks the newly created Test token', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: 'imported-account',
-        ganacheOptions,
-        title: this.test.title,
-      },
-      async ({ driver }) => {
-        await driver.navigate();
-        await driver.fill('#password', 'correct horse battery staple');
-        await driver.press('#password', driver.Key.ENTER);
-
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        await driver.closeAllWindowHandlesExcept([extension]);
-        await driver.clickElement('.app-header__logo-container');
-
-        // connects the dapp
-        await driver.openNewPage('http://127.0.0.1:8080/');
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(3);
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        dapp = await driver.switchToWindowWithTitle(
-          'E2E Test Dapp',
-          windowHandles,
-        );
-        popup = windowHandles.find(
-          (handle) => handle !== extension && handle !== dapp,
-        );
-
-        await driver.switchToWindow(popup);
-        await driver.clickElement({ text: 'Next', tag: 'button' });
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(2);
+    await driver.switchToWindow(popup);
+    await driver.clickElement({ text: 'Next', tag: 'button' });
+    await driver.clickElement({ text: 'Connect', tag: 'button' });
+    await driver.waitUntilXWindowHandles(2);
         await driver.switchToWindow(dapp);
         // create token
         await driver.waitForSelector({ text: 'Create Token', tag: 'button' });
@@ -212,86 +90,8 @@ describe('Add a custom token from a dapp', function () {
         await driver.fill('#custom-address', tokenAddress);
         await driver.clickElement({ text: 'Add Custom Token', tag: 'button' });
         await driver.clickElement({ text: 'Import Tokens', tag: 'button' });
-      },
-    );
-  });
-
-  it('renders the balance for the new token', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: 'imported-account',
-        ganacheOptions,
-        title: this.test.title,
-      },
-      async ({ driver }) => {
-        await driver.navigate();
-        await driver.fill('#password', 'correct horse battery staple');
-        await driver.press('#password', driver.Key.ENTER);
-
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        await driver.closeAllWindowHandlesExcept([extension]);
-        await driver.clickElement('.app-header__logo-container');
-
-        // connects the dapp
-        await driver.openNewPage('http://127.0.0.1:8080/');
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(3);
-        windowHandles = await driver.getAllWindowHandles();
-        extension = windowHandles[0];
-        dapp = await driver.switchToWindowWithTitle(
-          'E2E Test Dapp',
-          windowHandles,
-        );
-        popup = windowHandles.find(
-          (handle) => handle !== extension && handle !== dapp,
-        );
-
-        await driver.switchToWindow(popup);
-        await driver.clickElement({ text: 'Next', tag: 'button' });
-        await driver.clickElement({ text: 'Connect', tag: 'button' });
-        await driver.waitUntilXWindowHandles(2);
-        await driver.switchToWindow(dapp);
-        // create token
-        await driver.waitForSelector({ text: 'Create Token', tag: 'button' });
-        await driver.clickElement({ text: 'Create Token', tag: 'button' });
-        windowHandles = await driver.waitUntilXWindowHandles(3);
-
-        popup = windowHandles[2];
-        await driver.switchToWindow(popup);
-        await driver.clickElement({ text: 'Edit', tag: 'button' });
-        const inputs = await driver.findElements('input[type="number"]');
-        const gasLimitInput = inputs[0];
-        const gasPriceInput = inputs[1];
-        await gasLimitInput.fill('4700000');
-        await gasPriceInput.fill('20');
-        await driver.waitForSelector({ text: 'Save', tag: 'button' });
-        await driver.clickElement({ text: 'Save', tag: 'button' });
-        await driver.clickElement({ text: 'Confirm', tag: 'button' });
-
-        await driver.switchToWindow(dapp);
-
-        const tokenContractAddress = await driver.waitForSelector({
-          css: '#tokenAddress',
-          text: '0x',
-        });
-        const tokenAddress = await tokenContractAddress.getText();
-
-        // imports custom token from extension
-        await driver.switchToWindow(extension);
-        await driver.clickElement(`[data-testid="home__asset-tab"]`);
-        await driver.clickElement({ text: 'import tokens', tag: 'a' });
-        await driver.clickElement({
-          text: 'Custom Token',
-          tag: 'button',
-        });
-
-        await driver.waitForSelector('#custom-address');
-        await driver.fill('#custom-address', tokenAddress);
-        await driver.clickElement({ text: 'Add Custom Token', tag: 'button' });
-        await driver.clickElement({ text: 'Import Tokens', tag: 'button' });
-
+  
+        //renders balance for newly created token
         await driver.waitForSelector('.app-header__logo-container');
         await driver.clickElement('.app-header__logo-container');
         await driver.clickElement({ tag: 'button', text: 'Assets' });
@@ -305,6 +105,7 @@ describe('Add a custom token from a dapp', function () {
   });
 });
 
+/*
 describe('Approves a custom token from dapp', function () {
   let windowHandles;
   let extension;
@@ -795,3 +596,4 @@ describe('Approves a custom token from dapp', function () {
     );
   });
 });
+*/
